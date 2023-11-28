88ca7d22762160ab8eaab7f303810ce1270a5be9
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-11-28 14:03:15 +0200

    Add

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in "abcdefghijklmnopqrstuvwxyz":
-    print(i, end="")
+for ch in range(97, 123):
+    print("{}".format(chr(ch)),end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for ch in range(97, 123):
+    if chr(ch) != 'q' and chr(ch) != 'e':
+        print("{}".format(chr(ch)), end="")
