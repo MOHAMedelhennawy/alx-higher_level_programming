25467e4abcee6d184124ad670fd24e87028aa084
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-11-28 15:58:14 +0200

    Add

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
         if 97 <= ord(ch) <= 122:
             ch = chr(ord(ch) - 32)
         print("{}".format(ch), end="")
-    print("",end="\n")
+    print("", end="\n")
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last = number % 10 if number > -1 else 10 - (number % 10) 
+    print("{}".format(last), end="")
+    return last
