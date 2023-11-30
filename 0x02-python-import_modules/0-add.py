ddc9d192d5d5fb25913cf7236ada66ab9a59ef4e
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-11-30 13:00:02 +0200

    Fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 from add_0 import add
+
 if __name__ == "__main__":
     a = 1
     b = 2
     value = add(a, b)
-    print(f"1 + 2 = {value}")
+    print(f"{a:d} + {b:d} = {value}")
