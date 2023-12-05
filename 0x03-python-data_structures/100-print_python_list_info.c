e33fcd776f3740b28cc3a9a37a3a67ba0b7ce235
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-05 16:32:15 +0200

    Explain

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,8 +5,20 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t size_p, allocated, idx = 0;
 	PyObject *element;
 
-	size_p = PyList_Size(p);
+	size_p = PyList_Size(p);            /*to get the size of list that *p point to*/
 	allocated = ((PyListObject *)p)->allocated;
+    /* (PyListObject *)p: is typecast to interpret the generic PyObject pointer p as a pointer to PyListObject */
+
+    /*====================================*/
+
+    /*(PyListObject *) to convert the PyObject pointer p to a pointer of type PyListObject.
+     By doing this, we are telling the compiler to treat p as a pointer to a PyListObject structure.*/
+
+    /*====================================*/
+
+    /*->allocated: This field represents the number of elements that have been allocated in the list.
+     It indicates the memory space that has been reserved for the list,
+     which can be larger than the actual number of elements present in the list.*/
 	printf("[*] Size of the Python List = %ld\n", size_p);
 	printf("[*] Allocated = %ld\n", allocated);
 	while (idx < size_p)
