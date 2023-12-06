d393205bdd66c18465763041d359c8ad56355864
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-06 23:48:39 +0200

    Add

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == "None":
         return None
     else:
         return max(a_dictionary.keys())
