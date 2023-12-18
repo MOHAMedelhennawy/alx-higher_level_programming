fc7c021cc4b05893dd2f3ee14af6f3627f5b0c1d
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-18 22:32:04 +0200

    Fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        res = fct(args[0], args[1])
+        res = fct(*args)
     except ZeroDivisionError as E:
         res = None
         print("Exception: {}".format(E), file=sys.stderr)
