a9fc553905f16a92947fac6ff8e4a9ac63968876
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-18 19:48:25 +0200

    Del

--- 0x05-python-exceptions/100-main.py
+++ 0x05-python-exceptions/100-main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+safe_print_integer_err = \
+    __import__('100-safe_print_integer_err').safe_print_integer_err
 
 value = 89
-has_been_print = safe_print_integer(value)
+has_been_print = safe_print_integer_err(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
 value = -89
-has_been_print = safe_print_integer(value)
+has_been_print = safe_print_integer_err(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
 value = "School"
-has_been_print = safe_print_integer(value)
+has_been_print = safe_print_integer_err(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
