bb98853072f61d646b0a56dd497a9083b44231c2
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-19 19:41:13 +0200

    Add

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a Class"""
+
+
+class Square:
+    """Define a Class Square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
