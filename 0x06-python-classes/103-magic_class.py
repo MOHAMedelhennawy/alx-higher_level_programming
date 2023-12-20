df8696053e7a38a821892798ada0ce46e29a5ff0
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2023-12-20 22:10:37 +0200

    Add

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import math
+
+
+class _MagicClass:
+    def __init__(self, radius):
+        self._MagicClass__radius = 0
+
+        if type(radius) is not int:
+            if type(radius) != float:
+                raise TypeError("radius must be a number")
+        else:
+            self._MagicClass__radius = radius
+            return None
+
+    def area(self):
+        radius = self._MagicClass__radius
+        result = (self._MagicClass__radius ** 2) * math.pi
+        return result
+    
+    def circumference(self):
+        result = (math.pi * 2) * self._MagicClass__radius
+        return result
