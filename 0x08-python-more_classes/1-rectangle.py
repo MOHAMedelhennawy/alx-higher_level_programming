66698037165bc2727c03f9a0b9c7c450ec02b48d
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-05 03:48:39 +0200

    Fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
