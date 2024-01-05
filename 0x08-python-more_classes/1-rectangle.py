2d39ef10cb3551aeb453836136044ae6e8d90a07
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-05 03:43:21 +0200

    Fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,42 +6,39 @@ class Rectangle:
     """Defines the blueprint of a rectangle.
 
     Attribute:
-        width: An integer indicating the width of the rectangle object.
-        height: An integer indicating the height of the rectangle object.
+        width: the width of the rectangle object.
+        height: the height of the rectangle object.
     """
-
 
     def __init__(self, width=0, height=0):
         """An object constructor method.
-
         Initiatilizes Rectangle with width and height.
 
         Args:
-            width: An integer representing object width.
-                  Has a default value of 0.
-            height: An integer representing object height.
-                  Has a default value of 0.
+            width: representing object width.
+            height: representing object height.
+
         """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Gets the width private attribute value.
+        """Gets the width object attribute
 
-        Returns:
+        Return:
             The width private attribute
         """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the width private attribute value.
+        """Sets the width object attribute.
 
         Validates the assignment of the width private attribute.
 
-        Arg:
-            value: the value to be set
+        Args:
+            value: the value to set to width
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -51,21 +48,21 @@ class Rectangle:
 
     @property
     def height(self):
-        """Gets the height private attribute value.
+        """Gets the height object attribute
 
-        Returns:
+        Return:
             The height private attribute
         """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the height private attribute value.
+        """Sets the height object attribute.
 
         Validates the assignment of the height private attribute.
 
-        Arg:
-            value: the value to be set
+        Args:
+            value: the value to set to height
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
