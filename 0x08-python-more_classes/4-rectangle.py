92cf566cf13b0fbee6653cb05cdfb9f6329fd43c
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-05 03:52:05 +0200

    Fix

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -18,8 +18,8 @@ class Rectangle:
             height: representing object height.
 
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
