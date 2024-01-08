e9e7fee411c697b6100993031aa039df97757691
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-08 14:37:15 +0200

    Add

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Empty BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """Empty Class"""
+    
+    ...
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write BaseGeometry Class do nothing"""
+
+
+class BaseGeometry:
+    """Write Empty class that just handle Exception"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
