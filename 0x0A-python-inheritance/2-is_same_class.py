19214d4083994a544e84154ecd4f46c72907c2e0
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-08 12:38:05 +0200

    FixCodeStyle

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,7 +7,7 @@ the specified class ; otherwise False.
 
 def is_same_class(obj, a_class):
     """Return Ture if an instance, otherwise Flase
-    
+
     Args:
         obj: as the object of class
         a_class:T
