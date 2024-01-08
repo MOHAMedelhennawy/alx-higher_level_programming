9a36e081e218625f6f9ffd8d5dd8c8b4955274ca
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-08 12:08:05 +0200

    Add

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,12 +4,12 @@ Creates a class inheriting from the list class.
 """
 
 
+
 class MyList(list):
-    """Class MyList inherits from list."""
+    """Subclassing frOm list itself"""
 
     def print_sorted(self):
-        """Prints the list, in ascending sort."""
-
+        """Print_sorted to print sorted list"""
         new_list = self[:]
         new_list.sort()
         print("{}".format(new_list))
