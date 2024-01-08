f739abb9c22a3eb728821332a6609ea0cddd13ab
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-08 16:59:20 +0200

    AddTask-10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module 10-square.
+Creates a square class.
+"""
+
+
+Rectangle = __import__("8-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Represents a Square.
+    Private instance attributes:
+        - size
+    Inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        """Initializes an instance.
+
+        Args:
+            - size: value of both width and height
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of Square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Presintation of Square Class"""
+        return f"[Rectangle] {self.__size}/{self.__size}"
