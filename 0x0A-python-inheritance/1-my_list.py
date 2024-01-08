bf9fe8b95fa1521da5c8b1b60871f2554c51b2a3
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-08 12:13:20 +0200

    Fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,12 +4,12 @@ Creates a class inheriting from the list class.
 """
 
 
-
 class MyList(list):
     """Subclassing frOm list itself"""
 
     def print_sorted(self):
         """Print_sorted to print sorted list"""
+
         new_list = self[:]
         new_list.sort()
         print("{}".format(new_list))
