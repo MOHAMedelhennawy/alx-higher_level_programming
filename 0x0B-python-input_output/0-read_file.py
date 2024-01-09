e20e9c04e0c1a4aa5e2d4b6ec2064fd84a15b724
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-09 12:29:58 +0200

    FixTask-0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,6 +12,15 @@ def read_file(filename=""):
         - filename: name of the file
     """
 
-    with open(filename) as f:
-        read_text = f.read()
-        print(read_text, end="")
+    with open(filename) as file:
+        print(file.read(), end="")
+
+
+    # 1- is no need to call file.close()
+    # 2- but using the with statement makes the code
+    #    compact and much more readable
+    # 3- With statment: avoiding bugs and leaks by
+    #    ensuring that a resource is properly released
+    #    when the code using the resource is completely
+    #    executed. with statement itself ensures proper
+    #    acquisition and release of resources.
