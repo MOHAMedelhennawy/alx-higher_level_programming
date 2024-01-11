9a28ba1366ff9fb6994e18590ed16c30e9dbc78f
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-11 20:49:14 +0200

    FixTask-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,13 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
+    load_from_json_file = __import__("6-load_from_json_file")\
+        .load_from_json_file
 
     file_name = "add_item.json"
     try:
         items = load_from_json_file(file_name)
-    except:
+    except FileNotFoundError:
         items = []
 
     items.extend(sys.argv[1:])
