2ea6a0b72b563647f9c7ca223df4fcd55c37fd36
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-11 18:41:59 +0200

    AddTask-3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 import json
+"""Funtion that convert a python obj to json string"""
+
 
 def to_json_string(my_obj):
+    """returns the JSON representation of an object
+    """
+
     return json.dumps(my_obj)
+# If you have python obj and want to convert to
+# json opj, you use json.dumbs(),
+# It takes the Python object as input and
+# returns a JSON string. and it's call Encoding (Serializing) JSON
