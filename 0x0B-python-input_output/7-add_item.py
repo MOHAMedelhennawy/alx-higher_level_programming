b6086a45429dbda8e1e5ac6162bdfa1100801b49
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-11 20:33:14 +0200

    FixTask-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,10 @@
 """Add all arguments to a Python list and save them to a file."""
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
 if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
     my_list = sys.argv
     file_name = "add_item.json"
