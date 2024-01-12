1bb1d08846d2c35e910af2238d3ad342b4b1de2b
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-12 11:53:39 +0200

    AddTask-8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function returns the dictionary description
+with simple data structure
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """Return description
+    with simple data structure
+    """
+
+    return json.dumps(obj, default=lambda o: o.__dict__)
