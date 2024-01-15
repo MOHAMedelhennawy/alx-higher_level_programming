59df69a0cf8eb7ae6bfa1523e176b30852d316e9
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-15 18:04:06 +0200

    Fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+"""Module base.
+Defines a Base class for other classes in the project.
+"""
+
 import json
 import os
 
 class Base:
+    """Class with:
+    Private class attribute: __nb_objects
+    """
 
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """Initialization of a Base instance.
+
+        Args:
+            - id: id of the instance
+        """
+
         self.id = None
 
         if id != None:
@@ -16,10 +30,31 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Returns a JSON representation of list_dictionaries.
+
+        Args:
+            - list_dictionaries: list of dicts
+
+        Returns: JSON representation of the list
+        """
+
         return json.dumps(list_dictionaries or [])
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of
+        list_objs to a file.
+
+        Args:
+            - list_objs: list of instances who inherits of Base
+        """
+        """
+        if type(list_objs) != list and list_objs is not None:
+            raise TypeError("list_objs must be a list of instances")
+        if any(issubclass(type(x), Base) is False for x in list_objs):
+            raise TypeError("list_objs must be a list of instances")
+        """
+
         list_data = cls.to_json_string([obj.to_dictionary() for obj in list_objs])   
         file_name = cls.__name__ + ".json"
         with open(file_name, "w") as file_obj:
@@ -27,11 +62,25 @@ class Base:
     
     @staticmethod
     def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string.
+
+        Args:
+            - json_string: string to convert to list
+        """
+
         return json.loads(json_string) if json_string and json_string.strip() else []
     
 
     @classmethod
     def create(cls, **dictionary):
+        """Returns an instance with all attributes already set.
+
+        Args:
+            - dictionary: used as **kwargs
+
+        Returns: instance created
+        """
+
         dummy = cls(1, 1) if cls.__name__ == 'Rectangle' else cls(1)
         dummy.update(**dictionary)
         return dummy
