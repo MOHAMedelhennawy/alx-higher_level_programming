b6ec25eec79d9e2a8067b73b97f474ae9b54ec4c
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-16 01:44:24 +0200

    Fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,8 @@ Each test has the number of the task,
 and the number of the test for that task
 (i.e 'test_17_0' for the first test of task 17)
 """
+
+
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,6 +5,8 @@ Each test has the number of the task,
 and the number of the test for that task
 (i.e 'test_17_0' for the first test of task 17)
 """
+
+
 import unittest
 import io
 from models.base import Base
