5e64451c8da5e0c6dcab9aa38f464869209afa91
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-02-15 09:32:54 +0200

    Fix_task-16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all records of the table second_table
-SELECT score, name FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table WHERE name!='' OR name IS NOT NULL ORDER BY score DESC;
