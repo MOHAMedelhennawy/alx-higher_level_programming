e6770fe0574d202005576faf9c5d7b8f7dd109a4
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-03-09 02:26:58 -0800

    Add_Task-1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- Crate user
--- Execute: cat 1-create_user.sql | mysql -hlocalhost -uroot -p
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
