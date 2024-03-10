f530c521ab5fab78a44e0e65c380f55645df1798
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-03-10 03:42:55 -0700

    Add_Task-10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title AS title,
 tv_show_genres.genre_id AS genre_id
-FROM tv_shows join tv_show_genres;
+FROM tv_shows JOIN tv_show_genres;
