cb0e6ddd164e13830c075b0b35518b11248a5597
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-03-11 20:08:04 -0700

    Add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,4 @@ const len = process.argv.length;
 
 if (len < 3) console.log('No argument');
 else if (len > 3) console.log('Argument found');
-else console.log('Argument found');
+else console.log('Arguments found');
