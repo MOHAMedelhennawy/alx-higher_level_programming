030c09d347a5ace689da6566c1895e0f86ba914a
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-03-11 20:33:37 -0700

    Add

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,11 @@ const len = parseInt(process.argv[2]);
 
 if (len) {
   for (let i = 0; i < len; i++) {
-  let row = ''
-  for (let j = 0; j < len; j++) {
-		  row += 'X';
-	  }
-  console.log(row);
+    let row = '';
+    for (let j = 0; j < len; j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
 } else {
   console.log('Missing size');
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (arg1 && arg2) {
+  console.log(add(arg1, arg2));
+} else {
+  console.log('NaN');
+}
