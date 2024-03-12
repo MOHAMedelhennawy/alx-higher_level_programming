d5ed365d6b4e2f383adfd205d8ca4f31c5ef0f12
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-03-11 17:51:35 -0700

    Fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-if (argv.length <= 2) {
+const len = process.argv.length;
+if (len <= 2) {
   console.log('No argument');
 } else {
   console.log('Argument found');
