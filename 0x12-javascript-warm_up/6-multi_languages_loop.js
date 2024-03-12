447f300944c6beb9790d31367c11d95e89bfb47a
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-03-11 20:10:55 -0700

    Add' :

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const texts = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const x in texts) {
+  console.log(texts[x]);
+}
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
 
-if(Number.isInteger(x)) { 
-  for (let i = 0; i < x; i++)
-  console.log('C is fun');
+if (Number.isInteger(x)) {
+  for (let i = 0; i < x; i++) { console.log('C is fun'); }
 } else {
   console.log('Missing number of occurrences');
 }
