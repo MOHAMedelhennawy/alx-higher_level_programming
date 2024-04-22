c0d1f0066847d537b15c3fc26479f2f6f1db0f97
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-23 01:59:31 +0200

    Add Task-3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length <= 2) console.log('No argument');
+if (!argv[2]) console.log('No argument');
 else console.log(argv[2]);
