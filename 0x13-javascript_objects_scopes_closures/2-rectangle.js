43f945f00bf7da5acc8764b14ed8417651be131c
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-23 16:49:07 +0200

    Add Task-2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+    if (w <= 0 || h <= 0) {
       return {};
     }
     this.width = w;
