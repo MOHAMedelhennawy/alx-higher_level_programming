102f7b6c4ff6790d8a77989c163e1ff00c12aeac
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-23 02:41:21 +0200

    Add Task-103

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+function incr () {
+  myObject.value++;
+}
+
+myObject.incr = incr;
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
