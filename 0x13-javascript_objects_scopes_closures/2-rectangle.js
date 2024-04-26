f2937145e74e3d81c8a0930896e075c1f3ab5695
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-26 17:15:59 +0300

    Fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w >= 0 || h >= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return;
     }
     this.width = w;
     this.height = h;
