c1e4dfdefbbe900359c1a1692f6d8e8e945abb71
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-26 18:17:46 +0300

    Add Task-10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that lists first State objects from the database hbtn_0e_6_usa
+using sqlalchemy
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3])
+                        )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name == argv[4])
+
+    try:
+        print(states[0].id)
+    except IndexError:
+        print("Not found")
