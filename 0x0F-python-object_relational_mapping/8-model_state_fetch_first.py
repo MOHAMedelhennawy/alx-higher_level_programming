8623279b706152443b4bd730a8957be823d68d98
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-26 17:38:02 +0300

    Fix

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
         .format(argv[1], argv[2], argv[3])
                         )
 
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
