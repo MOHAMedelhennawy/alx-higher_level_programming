f0b47022cc45b9ec578f0153b851b6a6bfe65780
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-28 11:32:19 +0300

    Fix CodeStyle

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,7 +12,7 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                        .format(argv[1], argv[2], argv[3])
+        .format(argv[1], argv[2], argv[3])
                         )
 
     Base.metadata.create_all(engine)
