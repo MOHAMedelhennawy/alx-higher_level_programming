ac7e7121f86a6840ef37262d00ec28d01de3c95d
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-29 09:30:32 +0300

    Add task-14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3])
+                        )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    filtered_cities = session.query(City).join(State).filter(City.state_id == State.id)
+    for city in filtered_cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,18 +3,16 @@
 Create simple table using sqlAlchmey
 """
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
-
-Base = declarative_base()
-
-
-class State(Base):
+class City(Base):
     '''
     Table has two columns id, name
     '''
-    __tablename__ = "states"
+    __tablename__ = "cities"
     id = Column(Integer(), primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
 
-    city = relationship("City", back_populates="state", cascade="all, delete")
+    state = relationship("State", back_populates='city')
