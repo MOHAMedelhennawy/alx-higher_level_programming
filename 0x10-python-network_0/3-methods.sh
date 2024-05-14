fef3013f0bcd7c64041d15851186f0ea5560604a
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-14 16:16:37 +0300

    Add Task-3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI 0.0.0.0:5000/route_4 | grep "Allow" | cut -d " " -f2-
+curl -sI $1 | grep "Allow" | cut -d " " -f2-
