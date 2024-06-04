9f1b508a6fdac1192eaaf3186077735a967308fd
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-04 04:03:36 +0300

    Add Task-1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-# Python script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response."""
+
 
 import urllib
 from urllib import request
 from sys import argv
 
-url = argv[1]
-with urllib.request.urlopen(url) as resp:
-    print(resp.getheader('X-Request-Id'))
+
+if __name__ == "__main__":
+    url = argv[1]
+    with urllib.request.urlopen(url) as resp:
+        print(resp.getheader('X-Request-Id'))
