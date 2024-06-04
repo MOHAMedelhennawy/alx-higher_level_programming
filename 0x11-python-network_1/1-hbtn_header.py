0b6238de7ad66b4729df33c8341699551621ce2d
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-04 04:01:20 +0300

    Add Task-1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@
 
 import urllib
 from urllib import request
+from sys import argv
 
-
-url = 'https://alx-intranet.hbtn.io'
+url = argv[1]
 with urllib.request.urlopen(url) as resp:
     print(resp.getheader('X-Request-Id'))
