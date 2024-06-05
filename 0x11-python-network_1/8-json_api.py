c40c02596ca8863348648ff5f49857056f7248f6
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-05 08:44:59 +0300

    Add Task-8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {'q': q}
+
+    r = requests.post(url, data=payload)
+    
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
