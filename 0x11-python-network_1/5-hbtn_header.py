d31640f8cd2f63d0ac9d46ae0fe38a00b5d361b3
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-05 07:29:29 +0300

    Add

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status
 """
-import requests, sys
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = argv[1]
     r = requests.get(url)
     print(r.headers['X-Request-Id'])
