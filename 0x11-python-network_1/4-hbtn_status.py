f7549fb8f263e1d3d6206173196371b29fed5941
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-05 07:21:18 +0300

    Add

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@
 import requests
 
 r = requests.get('https://alx-intranet.hbtn.io/status')
-print("Body response:$\n\
+print("Body response:\n\
     - type: {}\n\
     - content: {}".format(type(r.text), r.text))
