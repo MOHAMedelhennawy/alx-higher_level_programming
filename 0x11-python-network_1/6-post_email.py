be8774c22bbda0252ffa2439e7e8ad2a294396db
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-05 08:24:00 +0300

    Add Task-6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
-import requests, sys
+import sys
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {'email': sys.argv[2]}
-    r = requests.post(url, data=value)
+    value = {"email": sys.argv[2]}
 
+    r = requests.post(url, data=value)
     print(r.text)
