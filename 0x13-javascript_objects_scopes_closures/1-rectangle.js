580693d591b4f45e1abc0e1b593e4f7eb3906b6e
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-08-25 06:14:29 +0300

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 class Rectangle {
   constructor (w, h) {
     this.width = w;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-  const revList = [];
-
-  for (const i in list) {
-    revList[i] = list[list.length - i - 1];
-  }
-  return revList;
-};
